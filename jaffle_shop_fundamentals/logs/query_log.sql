0db5ee22f213b178dcfe9715f2777367362096cb
laboneAlex <ojecty@gmail.com>
2026-08-02 00:30:39 +0200

    delete fundamental

---
+++ jaffle_shop_fundamentals/logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-08-01T21:58:26.740425500+00:00
+-- finished_at: 2026-08-01T21:58:27.118659700+00:00
+-- elapsed: 378ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c61b66-0107-a802-0005-36a2001cb72e
+-- desc: not provided
+select 1 as id;
